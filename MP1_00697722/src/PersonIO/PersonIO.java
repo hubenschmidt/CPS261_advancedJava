38f7d3a9f9c580238594f3baf9c0662d2338bb32
hubenschmidt <whubenschmidt@gmail.com>
2021-01-22 03:09:21 -0500

    adding mp1

--- MP1_00697722/src/PersonIO/PersonIO.java
+++ MP1_00697722/src/PersonIO/PersonIO.java
@@ -1,6 +1,5 @@
 package PersonIO;
 
-import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -8,6 +7,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Person implements Serializable {
@@ -37,82 +38,101 @@ public class PersonIO {
 	String fileName;
 	ObjectInputStream ois = null;
 	ObjectOutputStream oos = null;
+	FileInputStream fis = null;
 	static Scanner kbInput = new Scanner(System.in);
 
-	public PersonIO(String fileName) throws FileNotFoundException, IOException {
+	public PersonIO(String fileName) {
 		this.fileName = fileName;
+//		ois = new ObjectInputStream(new FileInputStream(this.fileName));
+//		oos = new ObjectOutputStream(new FileOutputStream(this.fileName, true));
+	}
 
-		ois = new ObjectInputStream(new FileInputStream(this.fileName));
+	public void writeToFile(List<Object> person) throws IOException {
 		oos = new ObjectOutputStream(new FileOutputStream(this.fileName));
+		oos.writeObject(person);
+		oos.close();
+	}
 
+	public void readFile() throws ClassNotFoundException, IOException {
+		ois = new ObjectInputStream(new FileInputStream(this.fileName));
+
+		List<Object> input = (List<Object>) ois.readObject();
+		List<Object> checkList = new ArrayList<>();
+
+//		for (int i = 0; i < checkList.size(); i++) {
+//			System.out.println(checkList.get(i));
+//		}
+
+		// this will contain the list of objects
+		for (Object l : input) {
+			checkList.add(l.getClass().getSimpleName());
+
+			if (l instanceof Person) {
+				Person p = (Person) l;
+				System.out.println(p.name);
+				System.out.println(p.age);
+			}
+
+		}
+//		System.out.println(checkList);
+		ois.close();
 	}
 
 	public void add(Person p) throws IOException {
-		oos.writeObject(p);
+		// built list of persons before writing to file
+		List<Object> peopleList = new ArrayList<>();
+		peopleList.add(p);
+		writeToFile(peopleList);
 		System.out.println("Added " + p.toString());
 	}
 
-	public void display() {
-		try {
-			Person p = (Person) ois.readObject();
-			System.out.println(p.toString());
-		} catch (EOFException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+//	public void display() {
+//		try {
+//			Person p = (Person) ois.readObject();
+//			System.out.println(p.toString());
+//		} catch (EOFException e) {
+//			e.printStackTrace();
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
 		PersonIO mp1 = new PersonIO("person.ser");
-		try {
-			int option = -1;
-			do {
-				System.out.println("Please choose an option:");
-				System.out.println("0: quit");
-				System.out.println("1: add");
-				System.out.println("2: display");
-				option = kbInput.nextInt();
-				kbInput.nextLine();
-				System.out.println("You entered: " + option);
-				switch (option) {
-				case 0:
-					System.out.println("Bye.");
-					break;
-				case 1:
-					System.out.println("Enter name: ");
-					String name = kbInput.nextLine();
-					System.out.println("Enter age: ");
-					int age = kbInput.nextInt();
-					Person p = new Person(name, age);
-					mp1.add(p);
-					break;
-				case 2:
-					mp1.display();
-					break;
-				default:
-					System.out.println(option + " is not a valid entry");
-				}
-
-			} while (option != 0);
-		} finally {
-			try {
-				mp1.ois.close();
-				mp1.oos.close();
-//				// close ois
-//				if (mp1.ois != null) {
-//					mp1.ois.close();
-//				}
-//				// close oos
-//				if (mp1.oos != null) {
-//					mp1.oos.close();
-//				}
-			} catch (IOException e) {
-				System.out.println("Error.");
-				e.printStackTrace();
+//		try {
+		int option = -1;
+		do {
+			System.out.println("Please choose an option:");
+			System.out.println("0: quit");
+			System.out.println("1: add");
+			System.out.println("2: display");
+			option = kbInput.nextInt();
+			kbInput.nextLine();
+			System.out.println("You entered: " + option);
+			switch (option) {
+			case 0:
+				System.out.println("Bye.");
+				break;
+			case 1:
+				System.out.println("Enter name: ");
+				String name = kbInput.nextLine();
+				System.out.println("Enter age: ");
+				int age = kbInput.nextInt();
+				Person p = new Person(name, age);
+				mp1.add(p);
+				break;
+			case 2:
+				mp1.readFile();
+//					mp1.display();
+				break;
+			default:
+				System.out.println(option + " is not a valid entry");
 			}
-		}
+
+		} while (option != 0);
 	}
 }
+//	}
+//}

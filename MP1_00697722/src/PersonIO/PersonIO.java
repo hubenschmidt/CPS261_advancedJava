b50782c15fc3ce36a87f3c5d9e4d950b23abae91
hubenschmidt <whubenschmidt@gmail.com>
2021-01-21 23:02:58 -0500

    adding mp1 PersonIO

--- MP1_00697722/src/PersonIO/PersonIO.java
+++ MP1_00697722/src/PersonIO/PersonIO.java
@@ -1,6 +1,5 @@
 package PersonIO;
 
-import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -11,8 +10,8 @@ import java.io.Serializable;
 import java.util.Scanner;
 
 class Person implements Serializable {
-	private String name;
-	private int age;
+	String name;
+	int age;
 
 	public Person(String name, int age) {
 		this.name = name;
@@ -22,22 +21,20 @@ class Person implements Serializable {
 	public String getName() {
 		return name;
 	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
 
 public class PersonIO {
-	private String fileName;
-	private ObjectInputStream ois = null;
-	private ObjectOutputStream oos = null;
-	private static Scanner kbInput = new Scanner(System.in);
+	String fileName;
+	ObjectInputStream ois = null;
+	ObjectOutputStream oos = null;
+	static Scanner kbInput = new Scanner(System.in);
 
 	public PersonIO(String fileName) throws FileNotFoundException, IOException {
 		this.fileName = fileName;
-		this.ois = new ObjectInputStream(new FileInputStream(this.fileName));
-		this.oos = new ObjectOutputStream(new FileOutputStream(this.fileName));
+
+		oos = new ObjectOutputStream(new FileOutputStream(this.fileName));
+		ois = new ObjectInputStream(new FileInputStream(this.fileName));
+
 	}
 
 	public void add(Person p) throws IOException {
@@ -45,21 +42,12 @@ public class PersonIO {
 		System.out.println("Added " + p.getName() + ".");
 	}
 
-	public void display() throws ClassNotFoundException, IOException, EOFException {
-
-		Person personRead = (Person) ois.readObject();
-		System.out.println(personRead);
-
+	public void display() {
+		System.out.println("Invoke display");
 	}
 
-	public static void main(String[] args) {
-		try {
-			PersonIO mp1 = new PersonIO("person.ser");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		PersonIO mp1 = new PersonIO("person");
 		try {
 			int option = -1;
 			while (option != 0) {
@@ -68,24 +56,40 @@ public class PersonIO {
 				System.out.println("1: add");
 				System.out.println("2: display");
 				option = kbInput.nextInt();
-				kbInput.nextLine();
 				switch (option) {
 				case 0:
 					System.out.println("Bye");
 					break;
 				case 1:
-					// invoke add
-					System.out.println("Enter a person's name");
+					System.out.println("Enter name: ");
+					String name = kbInput.nextLine();
+					System.out.println("Enter age: ");
+//					mp1.add(kbInput.nextLine());
+
+					break;
 				case 2:
-					// invoke display
+					mp1.display();
+
 				}
 
 			}
 		} finally {
-			// close oos
-			// close ois
+			try {
+				// close ois
+				if (mp1.ois != null) {
+					mp1.ois.close();
+				}
+				// close oos
+				if (mp1.oos != null) {
+					mp1.oos.close();
+				}
+
+			} catch (IOException e) {
+				System.out.println("Error.");
+				e.printStackTrace();
+			}
+
 		}
 
 	}
-
 }

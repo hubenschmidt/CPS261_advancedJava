f6509948d800b277f76deb18208f118beb83607c
hubenschmidt <whubenschmidt@gmail.com>
2021-01-31 03:40:50 -0500

    adding mp2

---
+++ MP2_00697722/src/Student/Main.java
@@ -0,0 +1,10 @@
+package Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("printing main");
+
+	}
+
+}
---
+++ MP2_00697722/src/Student/Student.java
@@ -0,0 +1,41 @@
+package Student;
+
+public class Student {
+	private int studentID = 10001;
+	private String name;
+	private double gpa;
+
+	public Student(int studentID, String name, double gpa) {
+		super();
+		this.studentID = studentID;
+		this.name = name;
+		this.gpa = gpa;
+	}
+
+	public int getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+	
+	main
+
+}

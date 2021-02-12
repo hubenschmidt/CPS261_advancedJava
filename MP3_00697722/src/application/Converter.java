38e7eec40e499a030fadb4c4eb275677c9b6d2ad
hubenschmidt <whubenschmidt@gmail.com>
2021-02-12 06:59:43 -0500

    adding mp3

--- MP3_00697722/src/application/Converter.java
+++ MP3_00697722/src/application/Converter.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -23,7 +24,7 @@ public class Converter extends Application {
 	Label labelB = new Label();
 
 	RadioButton temperature, distance, weight;
-	ToggleGroup sumGroup;
+	ToggleGroup toggleGroup;
 
 	public void setConversion(String conversion) {
 		switch (conversion) {
@@ -48,56 +49,14 @@ public class Converter extends Application {
 		}
 	}
 
-	public void makeGrid(GridPane gridPane, TextField english, TextField metric, Button eToM, Button mToE, Label labelA,
-			Label labelB) {
-		gridPane.add(labelA, 0, 0);
-		gridPane.add(english, 0, 1);
-		gridPane.add(labelB, 1, 0);
-		gridPane.add(metric, 1, 1);
-		gridPane.add(eToM, 0, 2);
-		gridPane.add(mToE, 1, 2);
-		gridPane.add(new Label("Select: "), 0, 3);
-		gridPane.add(temperature, 1, 3);
-		gridPane.add(distance, 1, 4);
-		gridPane.add(weight, 1, 5);
-	}
-
-	@Override
-	public void start(Stage primaryStage) {
-		GridPane gridPane = new GridPane();
-		gridPane.setVgap(5);
-		gridPane.setHgap(5);
-
-		// Makes Button group
-		sumGroup = new ToggleGroup();
-
-		// Adding all radio buttons
-		temperature = new RadioButton("Temperature");
-		temperature.setToggleGroup(sumGroup);
-
-		distance = new RadioButton("Distance");
-		distance.setToggleGroup(sumGroup);
-
-		weight = new RadioButton("Weight");
-		weight.setToggleGroup(sumGroup);
-
-		makeGrid(gridPane, english, metric, eToM, mToE, labelA, labelB);
-
-		sumGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
-			@Override
-			public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1) {
-				RadioButton chk = (RadioButton) t1.getToggleGroup().getSelectedToggle();
-				String conversion = chk.getText();
-				setConversion(conversion);
-			}
-		});
-
-		Scene scene = new Scene(gridPane, 400, 400);
-
-		primaryStage.setTitle("Converter");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+	/**
+	 * Convert english to metric units
+	 * 
+	 * @param eToM
+	 * @param mToE
+	 * @param english
+	 * @param metric
+	 */
 
 	public void convertTemperature(Button eToM, Button mToE, TextField english, TextField metric) {
 		eToM.setOnAction(e -> {
@@ -135,6 +94,60 @@ public class Converter extends Application {
 			double pounds;
 			pounds = (Double.parseDouble(metric.getText()) * 2.205);
 			english.setText(String.valueOf(pounds));
+		});
+	}
+
+	public void displayGrid(GridPane gridPane, TextField english, TextField metric, Button eToM, Button mToE,
+			Label labelA, Label labelB) {
+		gridPane.setVgap(5);
+		gridPane.setHgap(5);
+		gridPane.setAlignment(Pos.CENTER);
+		gridPane.add(labelA, 0, 0);
+		gridPane.add(english, 0, 1);
+		gridPane.add(labelB, 1, 0);
+		gridPane.add(metric, 1, 1);
+		gridPane.add(eToM, 0, 2);
+		gridPane.add(mToE, 1, 2);
+		gridPane.add(new Label("Select: "), 0, 3);
+		gridPane.add(temperature, 1, 3);
+		gridPane.add(distance, 1, 4);
+		gridPane.add(weight, 1, 5);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		GridPane gridPane = new GridPane();
+
+		ToggleGroup toggleGroup = new ToggleGroup();
+		temperature = new RadioButton("Temperature");
+		temperature.setToggleGroup(toggleGroup);
+		temperature.setSelected(true);
+		distance = new RadioButton("Distance");
+		distance.setToggleGroup(toggleGroup);
+		weight = new RadioButton("Weight");
+		weight.setToggleGroup(toggleGroup);
+
+		displayGrid(gridPane, english, metric, eToM, mToE, labelA, labelB);
+
+		Scene scene = new Scene(gridPane, 400, 400);
+
+		primaryStage.setTitle("English to Metric Converter");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		// initialize application with temperature conversion as default selection
+		RadioButton initialToggle = (RadioButton) toggleGroup.getSelectedToggle();
+		String initVal = initialToggle.getText();
+		setConversion(initVal);
+
+		// listen for radio button selection
+		toggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+			@Override
+			public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1) {
+				RadioButton chk = (RadioButton) t1.getToggleGroup().getSelectedToggle();
+				String conversion = chk.getText();
+				setConversion(conversion);
+			}
 		});
 	}
 

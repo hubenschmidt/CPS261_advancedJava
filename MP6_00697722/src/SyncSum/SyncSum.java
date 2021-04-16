dda1884911cca50abebf2840f9274df58b18e16e
hubenschmidt <williamhubenschmidt@Williams-iMac.gha.chartermi.net>
2021-04-16 17:14:44 -0400

    adding mp6

--- MP6_00697722/src/SyncSum/SyncSum.java
+++ MP6_00697722/src/SyncSum/SyncSum.java
@@ -1,27 +1,26 @@
 package SyncSum;
 
-class MySum extends Thread {
+class MySum {
     static int sum = 0;
 
-    synchronized private static void increaseSum(Thread t) {
+//    synchronized private static void increaseSum() { // method sync
+    public void increaseSum() {
 	try {
-	    Thread.sleep(100);
-	    sum++;
 
-	    System.out.println(t.getName() + " sum is: " + sum);
+	    /*
+	     * use with method sync or with unsynchronized demo:
+	     */
+//	    Thread.sleep(100);
+//	    sum++;
+//	    System.out.println(Thread.currentThread().getName() + " sum is: " + sum);
 
+	    synchronized (this) { // block sync
+		Thread.sleep(100);
+		sum++;
+		System.out.println(Thread.currentThread().getName() + " sum is: " + sum);
+	    }
 	} catch (InterruptedException e) {
 	    e.printStackTrace();
-	}
-
-    }
-
-    public void run() {
-
-	for (int i = 0; i < 10; i++) {
-	    Thread t = new Thread();
-	    increaseSum(t);
-
 	}
 
     }
@@ -29,10 +28,19 @@ class MySum extends Thread {
 }
 
 public class SyncSum {
+    static int numThreads = 100;
 
     public static void main(String[] args) {
 	MySum mySum = new MySum();
-	mySum.start();
+	for (int i = 0; i < numThreads; i++) {
+	    Runnable runnable = () -> {
+		mySum.increaseSum();
+	    };
+	    Thread t = new Thread(runnable);
+
+	    t.start();
+
+	}
 
     }
 
--- Unit6/src/lessons/ThreadProcessDemo.java
+++ Unit6/src/lessons/ThreadProcessDemo.java
@@ -1,32 +1,35 @@
 package lessons;
 
 class CPUWaster extends Thread {
-	public void run() {
-		while (true) {}
+    public void run() {
+	while (true) {
+	    System.out.println(Thread.currentThread().getName());
 	}
+    }
 }
+
 public class ThreadProcessDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        // display current information about this process
-        Runtime rt = Runtime.getRuntime();
-        long usedKB = (rt.totalMemory() - rt.freeMemory()) / 1024 ;
-        System.out.format("  Process ID: %d\n", ProcessHandle.current().pid());
-        System.out.format("Thread Count: %d\n", Thread.activeCount());
-        System.out.format("Memory Usage: %d KB\n", usedKB);
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	// display current information about this process
+	Runtime rt = Runtime.getRuntime();
+	long usedKB = (rt.totalMemory() - rt.freeMemory()) / 1024;
+	System.out.format("  Process ID: %d\n", ProcessHandle.current().pid());
+	System.out.format("Thread Count: %d\n", Thread.activeCount());
+	System.out.format("Memory Usage: %d KB\n", usedKB);
 
-        // start 6 new threads
-        System.out.println("\nStarting 2 CPUWaster threads...\n");
-        for (int i=0; i<1; i++)
-            new CPUWaster().start();
+	// start 6 new threads
+	System.out.println("\nStarting 2 CPUWaster threads...\n");
+	for (int i = 0; i < 1; i++)
+	    new CPUWaster().start();
 
-        // display current information about this process
-        usedKB = (rt.totalMemory() - rt.freeMemory()) / 1024 ;
-        System.out.format("  Process ID: %d\n", ProcessHandle.current().pid());
-        System.out.format("Thread Count: %d\n", Thread.activeCount());
-        System.out.format("Memory Usage: %d KB\n", usedKB);
+	// display current information about this process
+	usedKB = (rt.totalMemory() - rt.freeMemory()) / 1024;
+	System.out.format("  Process ID: %d\n", ProcessHandle.current().pid());
+	System.out.format("Thread Count: %d\n", Thread.activeCount());
+	System.out.format("Memory Usage: %d KB\n", usedKB);
 
-	}
+    }
 
 }

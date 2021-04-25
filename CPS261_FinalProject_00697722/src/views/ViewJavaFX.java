74f7aad932f8e8b8780feb52c4bd8dc6934498ae
hubenschmidt <williamhubenschmidt@Williams-iMac.gha.chartermi.net>
2021-04-25 06:53:54 -0400

    adding

--- CPS261_FinalProject_00697722/src/views/ViewJavaFX.java
+++ CPS261_FinalProject_00697722/src/views/ViewJavaFX.java
@@ -82,7 +82,8 @@ public class ViewJavaFX {
     private TableColumn winLoseCol = new TableColumn("Win");
 
     // create wins display
-    private Text displayWins;
+    private Text displayWinsP1;
+    private Text displayWinsP2;
 
     private int i;
 
@@ -194,33 +195,41 @@ public class ViewJavaFX {
 	exitHistory();
     }
 
+    /*
+     * gets and displays win total
+     */
+
     public void displayPlayerWins() {
-//	Map<String, Long> p1winsAndLosses = new LinkedHashMap<String, Long>();
-//	p1winsAndLosses.putAll(controller.computeTotalWins());
-
-//	System.out.println(p1winsAndLosses);
-
-	System.out.println(controller.computeTotalWins());
-
 	String p1Name = controller.getPlayers().get(0).getName();
 	String p2Name = controller.getPlayers().get(1).getName();
+	long p1Wins = 0;
+	long p2Wins = 0;
 
-	if (controller.computeTotalWins().keySet().contains("win")) {
-	    System.out.println(controller.computeTotalWins().values().toArray()[0]);
+	/*
+	 * controller.computeTotalWins() returns a Java 8 stream of LinkedHashMap. For
+	 * example, it may return any of the following: {loss=1, win=2}, {loss=1},
+	 * {win=1}, or {win=2}. if either key "win" or "loss" is null, it does *not*
+	 * return {loss=0, win=1} or {loss=1, win=0}. and so, use conditional logic
+	 * based on player 1 loss/win data to get wins for player 2.
+	 */
 
+	if (controller.computeTotalWins().keySet().contains("win")
+		&& !controller.computeTotalWins().keySet().contains("loss")) {
+	    p1Wins = (long) controller.computeTotalWins().values().toArray()[0];
+	    System.out.println("William has " + p1Wins + " wins.");
+	} else if (controller.computeTotalWins().keySet().contains("loss")
+		&& !controller.computeTotalWins().keySet().contains("win")) {
+	    p2Wins = (long) controller.computeTotalWins().values().toArray()[0];
+	    System.out.println("Jen has " + p2Wins + " wins.");
+	} else {
+	    p1Wins = (long) controller.computeTotalWins().values().toArray()[1];
+	    p2Wins = (long) controller.computeTotalWins().values().toArray()[0];
+	    System.out.println("William has " + p1Wins + " wins.");
+	    System.out.println("Jen has " + p2Wins + " wins.");
 	}
-//
-//	String p1Wins = p1winsAndLosses.entrySet().stream().reduce((first, second) -> second).orElse(null).getValue()
-//		.toString();
-//	String p2Wins = p1winsAndLosses.entrySet().stream().reduce((first, second) -> first).orElse(null).getValue()
-//		.toString();
-
-//	displayWins = new Text(p1Name + "wins");
-//	System.out.println(displayWins);
-//	displayWins = new Text(p2Name + " has" + p2Wins + "wins");
-//	System.out.println(displayWins);
-//	stackPane.getChildren().add(displayWins);
-
+	displayWinsP1 = new Text("William has " + p1Wins + " wins.");
+	displayWinsP2 = new Text("Jen has " + p2Wins + " wins.");
+	stackPane.getChildren().addAll(displayWinsP1, displayWinsP2);
     }
 
     public void exitHistory() {
